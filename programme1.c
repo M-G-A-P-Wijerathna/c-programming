28fbf3efe37dd12a06f51cef5b8fa2dc49cd1255
anjalika <anjalikawijerathna316@gmail.com>
2024-12-01 09:21:22 +0530

    \t \n

---
+++ programme1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+
+{
+    //comment 1
+
+
+    printf("welcome\t\t to programme one\n");
+   printf("new line ");
+
+   /* comment  line 1
+      comment line 2
+      comment line 3 */
+
+    return ;
+}
