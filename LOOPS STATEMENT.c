801a9fde556e13122024012df69314812ee04374
M-G-A-P-Wijerathna <anjalikawijerathna316@gmail.com>
2024-12-08 01:03:00 +0530

    for loop
    display the numbers from 1 to 10

---
+++ LOOPS STATEMENT.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    //display the numbers from 1 to 10 line by line
+    int x ;
+
+    for (x=1;x<11 ;x++)
+    {
+        printf("%d\n",x);
+    }
+
+
+
+
+    return 0;
+}
