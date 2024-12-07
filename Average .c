501762f752b79ace84be9f6882e8fcbaaea5ca8c
M-G-A-P-Wijerathna <anjalikawijerathna316@gmail.com>
2024-12-07 12:22:09 +0530

    Average

---
+++ Average .c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+  int num1,num2,num3, ave ;
+  printf("Enter first number - ");
+  scanf("%d",&num1);
+
+  printf("\nEnter second number - ");
+  scanf("%d",&num2);
+
+  printf("\nEnter third number - ");
+  scanf("%d",&num3);
+
+  ave = (num1 +num2+num3)/3 ;
+  printf("Average = %d", ave);
+
+
+
+
+
+
+    return 0;
+}
